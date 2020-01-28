0e02256cf858af9a4a7d0199b2c86676311ebdca
Yamamuro Saeru <saeru.yamamuro01@g.softbank.co.jp>
2020-01-28 14:01:53 +0900

    [modified] some files

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 from matplotlib.lines import Line2D
 
-from utils import draw_pitches
+from utils import *
 
 cmap = plt.get_cmap('tab10')
 
@@ -67,62 +67,65 @@ events_df = read_events_df(selected_competition)
 
 matchPeriod_list = st.multiselect('Which Half do you want to see data?', ['1H', '2H'])
 
-st.header('shot point')
-fig, axes = draw_pitches(nrows=len(matchPeriod_list), ncols=2)
+library = st.selectbox('Which Library do you want to visualize?', ['matplotlib', 'plotly'])
 
 try:
 	df_tmp = events_df[(events_df.matchId == selected_wyId)&(events_df.eventName=='Shot')&(events_df.matchPeriod.isin(matchPeriod_list))]
+	st.header('shot point')
 
-	for i, (matchPeriod, teamId) in enumerate(df_tmp[['matchPeriod', 'teamId']].drop_duplicates().sort_values(by=['teamId', 'matchPeriod']).values.tolist()):
-	    ax = axes.reshape([-1, 2])[int(i/2), i%2]
-	    team_name = teams_df[teams_df.wyId==teamId].name.values[0]
-	    ax.set_title(f'{matchPeriod} {team_name}', color='white')
-	    
-	    for (positions, tags) in df_tmp[(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)][['positions', 'tags']].values.tolist():
-	        id_list = [tag_dict['id'] for tag_dict in tags]
-	    
-	        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
-	        
-	        ax.scatter(x=st_x, y=st_y, c = cmap(1) if 101 in id_list else cmap(0), label='success' if 101 in id_list else 'fail')
+	if library == 'matplotlib':
+		fig, axes = draw_pitches_matplotlb(nrows=len(matchPeriod_list), ncols=2)
+		for i, (matchPeriod, teamId) in enumerate(df_tmp[['matchPeriod', 'teamId']].drop_duplicates().sort_values(by=['teamId', 'matchPeriod']).values.tolist()):
+		    ax = axes.reshape([-1, 2])[int(i/2), i%2]
+		    team_name = teams_df[teams_df.wyId==teamId].name.values[0]
+		    ax.set_title(f'{matchPeriod} {team_name}', color='white')
+		    
+		    for (positions, tags) in df_tmp[(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)][['positions', 'tags']].values.tolist():
+		        id_list = [tag_dict['id'] for tag_dict in tags]
+		    
+		        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
+		        
+		        ax.scatter(x=st_x, y=st_y, c = cmap(1) if 101 in id_list else cmap(0), label='success' if 101 in id_list else 'fail')
 
-	    if i == len(matchPeriod_list)*2-1:
-	    	ax.legend([Line2D([0], [0], color=cmap(j)) for j in range(2)], ['success', 'failure'], loc='lower right')
+		    if i == len(matchPeriod_list)*2-1:
+		    	ax.legend([Line2D([0], [0], color=cmap(j)) for j in range(2)], ['success', 'failure'], loc='lower right')
 
-	st.pyplot(fig, facecolor=fig.get_facecolor(), bbox_inches = 'tight')
+		st.pyplot(fig, facecolor=fig.get_facecolor(), bbox_inches = 'tight')
 
 	st.header('pass map')
-	fig, axes = draw_pitches(nrows=len(matchPeriod_list), ncols=2)
 
 	subEventName_list = events_df[events_df.eventName=='Pass'].subEventName.unique().tolist()
 	df_tmp = events_df[(events_df.matchId==selected_wyId)&(events_df.eventName=='Pass')&(events_df.matchPeriod.isin(matchPeriod_list))]
 
-	for i, (matchPeriod, teamId) in enumerate(df_tmp[['matchPeriod', 'teamId']].drop_duplicates().sort_values(by=['teamId', 'matchPeriod']).values.tolist()):
-	    ax = axes.reshape([-1, 2])[int(i/2), i%2]
-	    team_name = teams_df[teams_df.wyId==teamId].name.values[0]
-	    ax.set_title(f'{matchPeriod} {team_name}', color='white')
+	if library == 'matplotlib':
+		fig, axes = draw_pitches_matplotlib(nrows=len(matchPeriod_list), ncols=2)
+		for i, (matchPeriod, teamId) in enumerate(df_tmp[['matchPeriod', 'teamId']].drop_duplicates().sort_values(by=['teamId', 'matchPeriod']).values.tolist()):
+		    ax = axes.reshape([-1, 2])[int(i/2), i%2]
+		    team_name = teams_df[teams_df.wyId==teamId].name.values[0]
+		    ax.set_title(f'{matchPeriod} {team_name}', color='white')
 
-	    for (positions, subEventName, tags) in df_tmp[(df_tmp.eventName=='Pass')&(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)][['positions', 'subEventName', 'tags']].values.tolist():
-	        ed_x, ed_y = XMAX * positions[1]['x'] / 100, YMAX * (1 - positions[1]['y'] / 100)
-	        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
-	        id_list = [tag_dict['id'] for tag_dict in tags]
+		    for (positions, subEventName, tags) in df_tmp[(df_tmp.eventName=='Pass')&(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)][['positions', 'subEventName', 'tags']].values.tolist():
+		        ed_x, ed_y = XMAX * positions[1]['x'] / 100, YMAX * (1 - positions[1]['y'] / 100)
+		        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
+		        id_list = [tag_dict['id'] for tag_dict in tags]
 
-	        ax.annotate('', xy=[ed_x, ed_y], xytext=[st_x, st_y],
-	                arrowprops=dict(shrink=0, width=0.5, headwidth=4, alpha=0.8,
-	                                headlength=5, connectionstyle='arc3',
-	                                facecolor=cmap(subEventName_list.index(subEventName)), edgecolor=cmap(subEventName_list.index(subEventName))
-	                                , linestyle= 'solid' if 1801 in id_list else 'dashed'
-	                               )
+		        ax.annotate('', xy=[ed_x, ed_y], xytext=[st_x, st_y],
+		                arrowprops=dict(shrink=0, width=0.5, headwidth=4, alpha=0.8,
+		                                headlength=5, connectionstyle='arc3',
+		                                facecolor=cmap(subEventName_list.index(subEventName)), edgecolor=cmap(subEventName_list.index(subEventName))
+		                                , linestyle= 'solid' if 1801 in id_list else 'dashed'
+		                               )
 
-	               )
+		               )
 
-	    for positions in df_tmp[(df_tmp.eventName=='Interruption')&(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)]['positions'].values.tolist():
-	        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
-	        ax.scatter(x=st_x, y=st_y, c='white', s=100, marker='x')
+		    for positions in df_tmp[(df_tmp.eventName=='Interruption')&(df_tmp.matchPeriod==matchPeriod)&(df_tmp.teamId==teamId)]['positions'].values.tolist():
+		        st_x, st_y = XMAX * positions[0]['x'] / 100, YMAX * (1 - positions[0]['y'] / 100)
+		        ax.scatter(x=st_x, y=st_y, c='white', s=100, marker='x')
 
 
-	    if i == len(matchPeriod_list)*2-1:
-	        ax.legend([Line2D([0], [0], color=cmap(j)) for j in range(len(subEventName_list))], subEventName_list, loc='lower right')
+		    if i == len(matchPeriod_list)*2-1:
+		        ax.legend([Line2D([0], [0], color=cmap(j)) for j in range(len(subEventName_list))], subEventName_list, loc='lower right')
 
-	st.pyplot(fig, facecolor=fig.get_facecolor(), bbox_inches = 'tight')
+		st.pyplot(fig, facecolor=fig.get_facecolor(), bbox_inches = 'tight')
 except Exception:
 	print('please select Match / Half')
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-twitter_color = '#841d26'
 COLOR = 'white'
 
 XMAX, XMIN = 120, 0
 YMAX, YMIN = 80, 0
 
-def draw_pitches(nrows, ncols, colorbar=False):
+def draw_pitches_matplotlib(nrows, ncols, colorbar=False):
+    twitter_color = '#841d26'
+    
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10*ncols, 8*nrows if colorbar else 7*nrows), facecolor=twitter_color)
     
     fig.patch.set_facecolor('#141d26')
@@ -49,3 +50,44 @@ def draw_pitches(nrows, ncols, colorbar=False):
         ax.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
 
     return fig, axes
+
+def draw_pitches_plotly(nrows, ncols, title_list=[], colorbar=False):
+    twitter_color = 'rgb(20,29,38)'
+
+    fig = make_subplots(rows=nrows, cols=ncols, subplot_titles=title_list, horizontal_spacing=0.05, vertical_spacing=0.05)
+    for i in range(1,nrows+1):
+        for j in range(1,ncols+1):
+            
+            line_list = [[[0,120], [0,0]], [[120,120], [0,80]], [[120,0], [80,80]], [[0,0], [80,0]], [[60,60], [0,80]]]
+            line_list += [[[0,18], [18,18]], [[18,18], [18,62]], [[18,0], [62,62]], [[0,6], [30,30]], [[6,6], [30,50]], [[6,0], [50,50]]]
+            line_list += [[[120, 102], [18,18]], [[102,102], [18,62]], [[102,120], [62,62]], [[120,114], [30,30]], [[114,114], [30,50]], [[114,120], [50,50]]]
+
+            for [x, y] in line_list:
+                fig.add_trace(go.Scatter(x=x, y=y,
+                                    mode='lines',
+                                    line=dict(color=COLOR, width=2.5),
+                                    showlegend=False,
+                                    hoverinfo='none'
+                                    )
+                             , row=i, col=j)
+
+            line_list = [[[0,0], [36,44]], [[120,120], [36,44]]]
+            for [x, y] in line_list:
+                fig.add_trace(go.Scatter(x=x, y=y,
+                                    mode='lines',
+                                    line=dict(color=COLOR, width=12.5),
+                                    showlegend=False,
+                                    hoverinfo='none'
+                                    )
+                             , row=i, col=j)
+
+            fig.add_shape(go.layout.Shape(type='circle', x0=60-12, y0=40-12, x1=60+12, y1=40+12, line_color=COLOR, line_width=2.5), row=i, col=j)
+            fig.update_xaxes(range=[-1, 120+1], visible=False, row=i, col=j)
+            fig.update_yaxes(range=[-1, 80+1], visible=False, row=i, col=j)
+    
+    fig.update_layout(go.Layout(width=120*10, height=80*10, plot_bgcolor=twitter_color, paper_bgcolor=twitter_color, autosize=True, margin=dict(l=10, r=10, t=20, b=10), legend=dict(font=dict(color=COLOR))))
+    
+    for i in fig['layout']['annotations']:
+        i['font'] = dict(size=15,color=COLOR)
+    
+    return fig
